e121af2e1c14b5a7d98b6f442077a28c460afb42
RithwikPaladugula09 <88891398+RithwikPaladugula09@users.noreply.github.com>
2022-05-20 12:48:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+from turtle import color
+from xml.etree.ElementPath import get_parent_map
+import folium
+import pandas
+
+
+data=pandas.read_csv("volcanoes.txt")
+lat=list(data["LAT"])
+lon=list(data["LON"])
+elev=list(data["ELEV"])
+
+def color_producer(elevation):
+    if elevation <1000:
+        return 'green'
+    elif 1000<=elevation<=2000:
+        return'blue'
+    else: 
+        return 'orange'
+
+
+map = folium.Map(location=[38.58,-99.09],zoom_start=6,tiles="Stamen Terrain")
+
+fgv=folium.FeatureGroup(name="Volcanoes")
+
+for lt,ln,el in zip(lat,lon,elev):
+    fgv.add_child(folium.CircleMarker(location=[lt,ln], radius= 6 ,popup= "elevation="+str(el)+"mtrs", fill_color=color_producer(el),color = 'black' , fill_opacity=0.7))
+
+fgp=folium.FeatureGroup(name="Population")
+
+
+fgp.add_child(folium.GeoJson(data=open('world.json','r', encoding='utf-8-sig').read(),
+style_function = lambda x:{'fillColor':'yellow' if x['properties']['POP2005']<10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] <20000000 else 'red'}))
+
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+map.save("Map1.html")
